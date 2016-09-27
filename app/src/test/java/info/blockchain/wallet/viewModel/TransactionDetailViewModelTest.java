77093dc298bfcedee4f56894b259374814cf7362
Adam Bennett <adam@blockchain.com>
2016-09-27 13:58:59 +0100

    fix(Transaction detail strings): Show correct direction on historic value

--- app/src/test/java/info/blockchain/wallet/viewModel/TransactionDetailViewModelTest.java
+++ app/src/test/java/info/blockchain/wallet/viewModel/TransactionDetailViewModelTest.java
@@ -208,7 +208,7 @@ public class TransactionDetailViewModelTest extends RxTest {
         // Arrange
         TestSubscriber<String> subscriber = new TestSubscriber<>();
         // Act
-        mSubject.getTransactionValueString("USD", new Tx("", "", "", 0D, 0L, new HashMap<>())).toBlocking().subscribe(subscriber);
+        mSubject.getTransactionValueString("USD", mTxSent).toBlocking().subscribe(subscriber);
         // Assert
         assertNotNull(subscriber.getOnNextEvents().get(0));
         subscriber.onCompleted();
@@ -220,7 +220,31 @@ public class TransactionDetailViewModelTest extends RxTest {
         // Arrange
         TestSubscriber<String> subscriber = new TestSubscriber<>();
         // Act
-        mSubject.getTransactionValueString("GBP", new Tx("", "", "", 0D, 0L, new HashMap<>())).toBlocking().subscribe(subscriber);
+        mSubject.getTransactionValueString("GBP", mTxReceived).toBlocking().subscribe(subscriber);
+        // Assert
+        assertNotNull(subscriber.getOnNextEvents().get(0));
+        subscriber.onCompleted();
+        subscriber.assertNoErrors();
+    }
+
+    @Test
+    public void getTransactionValueStringReceived() {
+        // Arrange
+        TestSubscriber<String> subscriber = new TestSubscriber<>();
+        // Act
+        mSubject.getTransactionValueString("USD", mTxReceived).toBlocking().subscribe(subscriber);
+        // Assert
+        assertNotNull(subscriber.getOnNextEvents().get(0));
+        subscriber.onCompleted();
+        subscriber.assertNoErrors();
+    }
+
+    @Test
+    public void getTransactionValueStringTransferred() {
+        // Arrange
+        TestSubscriber<String> subscriber = new TestSubscriber<>();
+        // Act
+        mSubject.getTransactionValueString("USD", mTxSent).toBlocking().subscribe(subscriber);
         // Assert
         assertNotNull(subscriber.getOnNextEvents().get(0));
         subscriber.onCompleted();
