1c011ed5d083fd65ce45fb19222203ed3b6a2858
Justin Tormey <justin@blockchain.com>
2017-02-20 17:34:07 -0500

    feature(Buy): load frontend in webview, communicate with js

--- app/src/main/java/piuk/blockchain/android/ui/buy/BuyActivity.java
+++ app/src/main/java/piuk/blockchain/android/ui/buy/BuyActivity.java
@@ -2,6 +2,11 @@ package piuk.blockchain.android.ui.buy;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+
 import info.blockchain.wallet.exceptions.MetadataException;
 import info.blockchain.wallet.metadata.Metadata;
 import info.blockchain.wallet.payload.PayloadManager;
@@ -11,8 +16,10 @@ import piuk.blockchain.android.databinding.ActivityBuyBinding;
 import piuk.blockchain.android.ui.base.BaseAuthActivity;
 import piuk.blockchain.android.util.annotations.Thunk;
 
-public class BuyActivity extends BaseAuthActivity {
+public class BuyActivity extends BaseAuthActivity implements ValueCallback<String> {
 
+    public static final String TAG = BuyActivity.class.getSimpleName();
+    private final String JS_INTERFACE_NAME = "android";
     private final int METADATA_TYPE_EXTERNAL = 3;
 
     @Thunk
@@ -24,19 +31,36 @@ public class BuyActivity extends BaseAuthActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_buy);
 
-        //Webview
-//        binding.webview.
+        WebView webview = binding.webview;
+        webview.addJavascriptInterface(new JsInterface(), JS_INTERFACE_NAME);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("http://localhost:8080/wallet/#/intermediate/");
+        webview.evaluateJavascript("console.log('in js land');", this);
 
-        //Wallet Credentials
-//        PayloadManager.getInstance().getPayload().getGuid()
-//        PayloadManager.getInstance().getPayload().getSharedKey()
-//        PayloadManager.getInstance().getTempPassword().toString()
-
+        /*
+            Wallet Credentials
+            PayloadManager.getInstance().getPayload().getGuid()
+            PayloadManager.getInstance().getPayload().getSharedKey()
+            PayloadManager.getInstance().getTempPassword().toString()
+        */
     }
 
-    //metadata string from getMetadata, and set with setMetadata
+    // metadata string from getMetadata, and set with setMetadata
     private Metadata getBuyMetadata() throws IOException, MetadataException {
         return new Metadata.Builder(PayloadManager.getInstance().getMasterKey(),
             METADATA_TYPE_EXTERNAL).build();
+    }
+
+    public void onReceiveValue(String value) {
+        Log.d(TAG, "Received JS value: " + value);
+    }
+}
+
+class JsInterface {
+    public static final String TAG = JsInterface.class.getSimpleName();
+
+    @JavascriptInterface
+    public void frontendInitialized() {
+        Log.d(TAG, "frontendInitialized");
     }
 }
