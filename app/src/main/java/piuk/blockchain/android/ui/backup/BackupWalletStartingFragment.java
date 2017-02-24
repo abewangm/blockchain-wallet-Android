6371cbd9ccdae39bd5358e39fc33a24edaffd5b0
Adam Bennett <adam@blockchain.com>
2017-02-24 16:00:28 +0000

    feat(Remove Backup PIN): Remove PIN check before displaying mnemonic.

--- app/src/main/java/piuk/blockchain/android/ui/backup/BackupWalletStartingFragment.java
+++ app/src/main/java/piuk/blockchain/android/ui/backup/BackupWalletStartingFragment.java
@@ -1,8 +1,6 @@
 package piuk.blockchain.android.ui.backup;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,12 +12,7 @@ import info.blockchain.wallet.payload.PayloadManager;
 import piuk.blockchain.android.R;
 import piuk.blockchain.android.databinding.FragmentBackupStartBinding;
 import piuk.blockchain.android.ui.account.SecondPasswordHandler;
-import piuk.blockchain.android.ui.auth.PinEntryActivity;
 import piuk.blockchain.android.util.annotations.Thunk;
-
-import static android.app.Activity.RESULT_OK;
-import static piuk.blockchain.android.ui.auth.PinEntryFragment.KEY_VALIDATING_PIN_FOR_RESULT;
-import static piuk.blockchain.android.ui.auth.PinEntryFragment.REQUEST_CODE_VALIDATE_PIN;
 
 public class BackupWalletStartingFragment extends Fragment {
 
@@ -54,10 +47,7 @@ public class BackupWalletStartingFragment extends Fragment {
                 });
 
             } else {
-                Intent intent = new Intent(getActivity(), PinEntryActivity.class);
-                intent.putExtra(KEY_VALIDATING_PIN_FOR_RESULT, true);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivityForResult(intent, REQUEST_CODE_VALIDATE_PIN);
+                loadFragment(new BackupWalletWordListFragment());
             }
         });
 
@@ -67,17 +57,9 @@ public class BackupWalletStartingFragment extends Fragment {
     @Thunk
     void loadFragment(Fragment fragment) {
         getFragmentManager().beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_NONE)
                 .replace(R.id.content_frame, fragment)
                 .addToBackStack(null)
                 .commit();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_VALIDATE_PIN && resultCode == RESULT_OK) {
-            loadFragment(new BackupWalletWordListFragment());
-        }
-    }
 }
