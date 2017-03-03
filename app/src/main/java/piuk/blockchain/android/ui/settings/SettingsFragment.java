fe9b766ba94f631e20e0ac7c72b7e9bf69bc90c5
Adam Bennett <adam@blockchain.com>
2017-03-03 14:54:58 +0000

    fix(Gradle script): More bug fixes, plus a Gradle script which will throw an error when creating release builds unless the expected google-services.json file is present

--- app/src/main/java/piuk/blockchain/android/ui/settings/SettingsFragment.java
+++ app/src/main/java/piuk/blockchain/android/ui/settings/SettingsFragment.java
@@ -30,6 +30,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -552,7 +553,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Prefer
         } else {
             LayoutInflater inflater = getActivity().getLayoutInflater();
             View smsPickerView = inflater.inflate(R.layout.include_sms_update, null);
-            AppCompatEditText mobileNumber = (AppCompatEditText) smsPickerView.findViewById(R.id.etSms);
+            EditText mobileNumber = (EditText) smsPickerView.findViewById(R.id.etSms);
             TextView countryTextView = (TextView) smsPickerView.findViewById(R.id.tvCountry);
             TextView mobileNumberTextView = (TextView) smsPickerView.findViewById(R.id.tvSms);
 
