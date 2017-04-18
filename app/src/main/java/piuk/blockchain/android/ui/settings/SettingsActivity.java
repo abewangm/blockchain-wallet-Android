3b08aa1fdc4c9c2db0c138a9fd5de24b5d4087d2
Adam Bennett <adam@blockchain.com>
2017-04-18 14:04:39 +0100

    fix(Suggest Merchant): Hide page, remove unneeded deps. Fixes for Travis.

--- app/src/main/java/piuk/blockchain/android/ui/settings/SettingsActivity.java
+++ app/src/main/java/piuk/blockchain/android/ui/settings/SettingsActivity.java
@@ -12,24 +12,16 @@ public class SettingsActivity extends BaseAuthActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_settings);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
         setupToolbar();
     }
 
     private void setupToolbar() {
-        //TODO - don't use NoActionBar in styles.xml (affects BalanceFragment, so don't just edit styles.xml)
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_general);
         setupToolbar(toolbar, R.string.action_settings);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
         toolbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_white_24dp));
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
+
 }
