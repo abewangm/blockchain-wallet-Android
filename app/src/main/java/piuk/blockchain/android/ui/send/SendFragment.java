359ece8a744b7f3337cb8749190a4ae9f749a0d1
Adam Bennett <adam@blockchain.com>
2017-05-02 15:57:01 +0100

    fix(Menus): Fix issue where menu items could duplicate

--- app/src/main/java/piuk/blockchain/android/ui/send/SendFragment.java
+++ app/src/main/java/piuk/blockchain/android/ui/send/SendFragment.java
@@ -146,11 +146,17 @@ public class SendFragment extends Fragment implements SendContract.DataListener,
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
         if (getArguments() != null) {
             selectedAccountPosition = getArguments().getInt(ARGUMENT_SELECTED_ACCOUNT_POSITION);
         }
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_send, container, false);
         viewModel = new SendViewModel(this, Locale.getDefault());
 
@@ -158,7 +164,6 @@ public class SendFragment extends Fragment implements SendContract.DataListener,
 
         setCustomKeypad();
         setupViews();
-        setHasOptionsMenu(true);
         viewModel.onViewReady();
 
         return binding.getRoot();
@@ -200,18 +205,9 @@ public class SendFragment extends Fragment implements SendContract.DataListener,
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        if (menu != null) menu.clear();
         inflater.inflate(R.menu.menu_send, menu);
         super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public void onPrepareOptionsMenu(Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        // Main activity QR button
-        final MenuItem menuItem = menu.findItem(R.id.action_qr_main);
-        if (menuItem != null) {
-            menuItem.setVisible(false);
-        }
     }
 
     @Override
