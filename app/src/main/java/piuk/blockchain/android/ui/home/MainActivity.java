8b5e0022b70dcad55e9c3c52f1babb593550d09c
Justin Tormey <justin@blockchain.com>
2017-05-22 16:24:02 -0400

    fix(Buy): disable buy on android api levels below 19

--- app/src/main/java/piuk/blockchain/android/ui/home/MainActivity.java
+++ app/src/main/java/piuk/blockchain/android/ui/home/MainActivity.java
@@ -726,7 +726,7 @@ public class MainActivity extends BaseAuthActivity implements BalanceFragment.On
 
     @Override
     public boolean isBuySellPermitted() {
-        return BuildConfig.BUY_BITCOIN_ENABLED;
+        return BuildConfig.BUY_BITCOIN_ENABLED && AndroidUtils.is19orHigher();
     }
 
     @Override
