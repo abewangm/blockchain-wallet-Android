a26ab2711c69ace4bee436ddf833beafcbc98f88
Adam Bennett <adam@blockchain.com>
2017-06-28 10:33:59 +0100

    fix(UI Tests): Re-enable tests, hopefully this should resolve the issues on Travis

--- app/src/androidTest/java/piuk/blockchain/android/ui/auth/LandingActivityTest.java
+++ app/src/androidTest/java/piuk/blockchain/android/ui/auth/LandingActivityTest.java
@@ -20,6 +20,7 @@ import static android.support.test.espresso.matcher.RootMatchers.isDialog;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static java.lang.Thread.sleep;
 import static junit.framework.TestCase.assertTrue;
 
 @LargeTest
@@ -39,32 +40,33 @@ public class LandingActivityTest extends BaseEspressoTest {
         assertTrue(activityRule.getActivity() != null);
     }
 
-    @Ignore
     @Test
     public void launchLoginPage() throws InterruptedException {
         BUTTON_LOGIN.perform(click());
+        sleep(500);
         // Check pairing fragment launched
         onView(withText(R.string.pair_your_wallet)).check(matches(isDisplayed()));
     }
 
-    @Ignore
     @Test
     public void launchCreateWalletPage() throws InterruptedException {
         BUTTON_CREATE.perform(click());
+        sleep(500);
         // Check create wallet fragment launched
         onView(withText(R.string.new_wallet)).check(matches(isDisplayed()));
     }
 
-    @Ignore
     @Test
     public void launchRecoverFundsPage() throws InterruptedException {
         BUTTON_RECOVER.perform(click());
+        sleep(500);
         // Verify warning dialog showing
         onView(withText(R.string.recover_funds_warning_message))
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()));
         // Click "Continue"
         onView(withId(android.R.id.button1)).perform(click());
+        sleep(500);
         // Check recover funds activity launched
         onView(withText(R.string.recover_funds)).check(matches(isDisplayed()));
     }
