ca75bec71776c219f89f77faf228019d71ae1e39
Adam Bennett <adam@blockchain.com>
2017-08-24 14:29:41 +0100

    feature(Ethereum): Some structural changes, add DTO for display

--- app/src/main/java/piuk/blockchain/android/data/ethereum/EthDataManager.kt
+++ app/src/main/java/piuk/blockchain/android/data/ethereum/EthDataManager.kt
@@ -1,58 +1,68 @@
 package piuk.blockchain.android.data.ethereum
 
+import info.blockchain.wallet.ethereum.EthAccountApi
 import info.blockchain.wallet.ethereum.data.EthAccount
+import info.blockchain.wallet.ethereum.data.EthTransaction
 import io.reactivex.Observable
-import piuk.blockchain.android.data.ethereum.datastore.EthDataStore
 import piuk.blockchain.android.data.rxjava.RxBus
 import piuk.blockchain.android.data.rxjava.RxPinning
 import piuk.blockchain.android.data.rxjava.RxUtil
 
 class EthDataManager(
-        private val ethService: EthService,
-        private val ethDataStore: EthDataStore,
+        private val ethAccountApi: EthAccountApi,
         rxBus: RxBus
 ) {
 
     private val rxPinning = RxPinning(rxBus)
+    private var address: String? = null
+    private var ethAccount: EthAccount? = null
 
     /**
      * Stores the ETH address associated with the user's wallet for the duration of the session.
      *
      * @param address The ETH address belonging to the logged in user
      */
-    fun initEthAccount(address: String) = ethService.storeEthAccount(address)
+    internal fun storeEthAccount(address: String) {
+        this.address = address
+    }
 
     /**
-     * Clears the currently stored ETH address from memory.
+     * Clears the currently stored ETH address and [EthAccount] from memory.
      */
-    fun clearEthAccount() = ethService.clearEthAccount()
+    internal fun clearEthAccount() {
+        address = null
+        ethAccount = null
+    }
 
     /**
      * Returns an [EthAccount] object for a given ETH address as an [Observable]. An [EthAccount]
      * contains a list of transactions associated with the account, as well as a final balance.
-     * Calling this function also stores the [EthAccount] in the [EthDataStore] for caching.
+     * Calling this function also caches the [EthAccount].
      *
      * @return An [Observable] wrapping an [EthAccount]
      */
     fun fetchEthAccount(): Observable<EthAccount> = rxPinning.call<EthAccount> {
-        ethDataStore.fetchEthAccount()
+        ethAccountApi.getEthAccount(address)
+                .doOnNext { account -> ethAccount = account }
                 .compose(RxUtil.applySchedulersToObservable())
     }
 
     /**
-     * Returns a list of [EthDisplayable] objects associated with a user's ETH address specifically
+     * Returns a list of [EthTransaction] objects associated with a user's ETH address specifically
      * for displaying in the transaction list. These are cached and may be empty if the account
      * hasn't previously been fetched.
      *
-     * @return An [Observable] list of [EthDisplayable] objects
+     * @return An [Observable] list of [EthTransaction] objects
      */
-    fun getEthTransactions(): Observable<List<EthDisplayable>> = rxPinning.call<List<EthDisplayable>> {
-        ethDataStore.getEthAccount()
-                .flatMapIterable { ethAccount -> ethAccount.transactions
-                            .map { EthDisplayable(ethAccount, it) }
-                }
-                .map { listOf(it) }
-                .compose(RxUtil.applySchedulersToObservable())
+    fun getEthTransactions(): Observable<List<EthTransaction>> {
+        ethAccount?.let {
+            return Observable.just(it)
+                    .flatMapIterable { it.transactions }
+                    .map { listOf(it) }
+                    .compose(RxUtil.applySchedulersToObservable())
+        }
+
+        return Observable.just(emptyList())
     }
 
     /**
@@ -64,7 +74,7 @@ class EthDataManager(
      * @return An [Observable] returning true or false based on the address's contract status
      */
     fun getIfContract(address: String): Observable<Boolean> = rxPinning.call<Boolean> {
-        ethService.getIfContract(address)
+        ethAccountApi.getIfContract(address)
                 .compose(RxUtil.applySchedulersToObservable())
     }
 
