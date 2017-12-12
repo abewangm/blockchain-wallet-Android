14af2d0b2811037334128033f712083b879bfd1f
Adam Bennett <adam@blockchain.com>
2017-12-12 16:22:55 +0000

    chore(Fastlane): Allow uploading builds to Crashlytics beta

--- scripts/ci_unit_tests.sh
+++ scripts/ci_unit_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./gradlew assembleEnvProdMinApi21Debug testEnvProdMinApi21DebugUnitTest testEnvProdMinApi21DebugUnitTestCoverage coveralls -Dpre-dex=false -Pkotlin.incremental=false --stacktrace
+./gradlew testEnvProdMinApi21DebugUnitTestCoverage coveralls -Dpre-dex=false -Pkotlin.incremental=false --stacktrace
