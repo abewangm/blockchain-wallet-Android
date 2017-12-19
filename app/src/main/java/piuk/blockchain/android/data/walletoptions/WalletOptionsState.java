762593b9dda71a030df52cec7d23e4d0a45fef0d
Adam Bennett <adam@blockchain.com>
2017-12-19 13:31:15 +0000

    feature(State Storage): Retrieve and store US state in metadata

--- app/src/main/java/piuk/blockchain/android/data/walletoptions/WalletOptionsState.java
+++ app/src/main/java/piuk/blockchain/android/data/walletoptions/WalletOptionsState.java
@@ -11,14 +11,10 @@ public class WalletOptionsState {
     ReplaySubject<WalletOptions> walletOptionsSource;
     ReplaySubject<Settings> walletSettingsSource;
 
-    private boolean americanStateSelectionRequired = false;
-    private String americanState;
-
     private WalletOptionsState(ReplaySubject<WalletOptions> walletOptionsSource,
                                      ReplaySubject<Settings> walletSettingsSource) {
         this.walletOptionsSource = walletOptionsSource;
         this.walletSettingsSource = walletSettingsSource;
-        americanState = null;
     }
 
     public static WalletOptionsState getInstance(ReplaySubject<WalletOptions> walletOptionsSubject,
@@ -32,19 +28,4 @@ public class WalletOptionsState {
         instance = null;
     }
 
-    public boolean isAmericanStateSelectionRequired() {
-        return americanStateSelectionRequired;
-    }
-
-    public void setAmericanStateSelectionRequired(boolean required) {
-        americanStateSelectionRequired = required;
-    }
-
-    public String getAmericanState() {
-        return americanState;
-    }
-
-    public void setAmericanState(String americanState) {
-        this.americanState = americanState;
-    }
 }
