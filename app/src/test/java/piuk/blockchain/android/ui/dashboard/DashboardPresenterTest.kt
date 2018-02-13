20c05c37266219b9dc36dcfe4d0cad9ae2c4a267
riaanjvos <riaan.vos@blockchain.com>
2018-02-13 14:33:51 +0000

    feat(SFOX): Add SFOX announcement tests.

--- app/src/test/java/piuk/blockchain/android/ui/dashboard/DashboardPresenterTest.kt
+++ app/src/test/java/piuk/blockchain/android/ui/dashboard/DashboardPresenterTest.kt
@@ -10,6 +10,7 @@ import com.nhaarman.mockito_kotlin.whenever
 import info.blockchain.wallet.prices.data.PriceDatum
 import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import org.amshove.kluent.any
 import org.amshove.kluent.mock
 import org.junit.Before
@@ -31,7 +32,14 @@ import piuk.blockchain.android.util.PrefsUtil
 import piuk.blockchain.android.util.StringUtils
 import java.math.BigInteger
 import java.util.*
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import java.util.concurrent.TimeUnit
 
+
+@Suppress("IllegalIdentifier")
 class DashboardPresenterTest {
 
     private lateinit var subject: DashboardPresenter
@@ -51,6 +59,8 @@ class DashboardPresenterTest {
 
     @Before
     fun setUp() {
+
+        setUpRxSchedulers()
 
         subject = DashboardPresenter(
                 prefsUtil,
@@ -72,6 +82,29 @@ class DashboardPresenterTest {
         whenever(view.locale).thenReturn(Locale.US)
         whenever(bchDataManager.getWalletTransactions(50, 0))
                 .thenReturn(Observable.just(emptyList()))
+    }
+
+    /*
+    Avoid RuntimeException when using 'observeOn(AndroidSchedulers.mainThread()'
+     */
+    fun setUpRxSchedulers() {
+        val immediate = object : Scheduler() {
+
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker{ it.run() }
+            }
+
+            override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+                // this prevents StackOverflowErrors when scheduling with a delay
+                return super.scheduleDirect(run, 0, unit)
+            }
+        }
+
+        RxJavaPlugins.setInitIoSchedulerHandler({ scheduler -> immediate })
+        RxJavaPlugins.setInitComputationSchedulerHandler({ scheduler -> immediate })
+        RxJavaPlugins.setInitNewThreadSchedulerHandler({ scheduler -> immediate })
+        RxJavaPlugins.setInitSingleSchedulerHandler({ scheduler -> immediate })
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> immediate }
     }
 
     @Test
@@ -194,7 +227,70 @@ class DashboardPresenterTest {
 
     @Test
     @Throws(Exception::class)
-    fun `onViewReady onboarding complete with announcement`() {
+    fun `onViewReady onboarding complete with bch and Sfox announcement`() {
+        // Arrange
+        val metadataObservable = Observable.just(MetadataEvent.SETUP_COMPLETE)
+        whenever(rxBus.register(MetadataEvent::class.java)).thenReturn(metadataObservable)
+        whenever(prefsUtil.getValue(PrefsUtil.KEY_ONBOARDING_COMPLETE, false))
+                .thenReturn(true)
+        whenever(appUtil.isNewlyCreated).thenReturn(false)
+        val combinedEthModel: CombinedEthModel = mock()
+        whenever(ethDataManager.fetchEthAddress()).thenReturn(Observable.just(combinedEthModel))
+        whenever(payloadDataManager.updateAllBalances()).thenReturn(Completable.complete())
+        whenever(payloadDataManager.updateAllTransactions()).thenReturn(Completable.complete())
+        val btcBalance = 21_000_000_000L
+        whenever(transactionListDataManager.getBtcBalance(any())).thenReturn(btcBalance)
+        val ethBalance = 22_000_000_000L
+        whenever(combinedEthModel.getTotalBalance()).thenReturn(BigInteger.valueOf(ethBalance))
+        whenever(prefsUtil.getValue(PrefsUtil.KEY_BTC_UNITS, MonetaryUtil.UNIT_BTC)).thenReturn(0)
+        whenever(prefsUtil.getValue(PrefsUtil.KEY_SELECTED_FIAT, PrefsUtil.DEFAULT_CURRENCY))
+                .thenReturn("USD")
+        whenever(exchangeRateFactory.getLastBtcPrice("USD")).thenReturn(2.0)
+        whenever(exchangeRateFactory.getLastEthPrice("USD")).thenReturn(3.0)
+        whenever(exchangeRateFactory.updateTickers())
+                .thenReturn(Observable.just(mapOf("" to PriceDatum())))
+        whenever(prefsUtil.getValue(DashboardPresenter.BITCOIN_CASH_ANNOUNCEMENT_DISMISSED, false))
+                .thenReturn(false)
+        whenever(prefsUtil.getValue(DashboardPresenter.SFOX_ANNOUNCEMENT_DISMISSED, false))
+                .thenReturn(false)
+        whenever(stringUtils.getString(any())).thenReturn("")
+        whenever(bchDataManager.updateAllBalances()).thenReturn(Completable.complete())
+        whenever(buyDataManager.canBuy).thenReturn(Observable.just(true))
+        // Act
+        subject.onViewReady()
+        // Assert
+        verify(view, atLeastOnce()).notifyItemAdded(any(), eq(0))
+        verify(exchangeRateFactory).updateTickers()
+        verify(view, atLeastOnce()).notifyItemUpdated(any(), any())
+        verify(view, atLeastOnce()).locale
+        verify(view, atLeastOnce()).updatePieChartState(any())
+        verify(prefsUtil, atLeastOnce()).getValue(PrefsUtil.KEY_ONBOARDING_COMPLETE, false)
+        verify(prefsUtil, atLeastOnce()).getValue(PrefsUtil.KEY_BTC_UNITS, MonetaryUtil.UNIT_BTC)
+        verify(prefsUtil, atLeastOnce()).getValue(PrefsUtil.KEY_SELECTED_FIAT, PrefsUtil.DEFAULT_CURRENCY)
+        verify(prefsUtil, atLeastOnce()).getValue(DashboardPresenter.BITCOIN_CASH_ANNOUNCEMENT_DISMISSED, false)
+        verify(prefsUtil, atLeastOnce()).setValue(DashboardPresenter.BITCOIN_CASH_ANNOUNCEMENT_DISMISSED, true)
+        verify(prefsUtil, atLeastOnce()).getValue(DashboardPresenter.SFOX_ANNOUNCEMENT_DISMISSED, false)
+        verify(prefsUtil, atLeastOnce()).setValue(DashboardPresenter.SFOX_ANNOUNCEMENT_DISMISSED, true)
+        verifyNoMoreInteractions(prefsUtil)
+        verify(ethDataManager).fetchEthAddress()
+        verifyNoMoreInteractions(ethDataManager)
+        verify(payloadDataManager).updateAllBalances()
+        verify(payloadDataManager).updateAllTransactions()
+        verifyNoMoreInteractions(payloadDataManager)
+        verify(transactionListDataManager).getBtcBalance(any())
+        verify(transactionListDataManager).getBchBalance(any())
+        verifyNoMoreInteractions(transactionListDataManager)
+        verify(exchangeRateFactory, times(3)).getLastBtcPrice("USD")
+        verify(exchangeRateFactory, times(3)).getLastEthPrice("USD")
+        verify(exchangeRateFactory, times(3)).getLastBchPrice("USD")
+        verify(exchangeRateFactory).updateTickers()
+        verifyNoMoreInteractions(exchangeRateFactory)
+        verify(bchDataManager, atLeastOnce()).updateAllBalances()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `onViewReady onboarding complete with bch but no Sfox announcement`() {
         // Arrange
         val metadataObservable = Observable.just(MetadataEvent.SETUP_COMPLETE)
         whenever(rxBus.register(MetadataEvent::class.java)).thenReturn(metadataObservable)
@@ -220,10 +316,12 @@ class DashboardPresenterTest {
                 .thenReturn(false)
         whenever(stringUtils.getString(any())).thenReturn("")
         whenever(bchDataManager.updateAllBalances()).thenReturn(Completable.complete())
+        whenever(buyDataManager.canBuy).thenReturn(Observable.just(false))
         // Act
         subject.onViewReady()
         // Assert
         verify(view, atLeastOnce()).notifyItemAdded(any(), eq(0))
+        verify(exchangeRateFactory).updateTickers()
         verify(view, atLeastOnce()).notifyItemUpdated(any(), any())
         verify(view, atLeastOnce()).locale
         verify(view, atLeastOnce()).updatePieChartState(any())
@@ -237,7 +335,6 @@ class DashboardPresenterTest {
         verifyNoMoreInteractions(ethDataManager)
         verify(payloadDataManager).updateAllBalances()
         verify(payloadDataManager).updateAllTransactions()
-        verify(payloadDataManager).wallet
         verifyNoMoreInteractions(payloadDataManager)
         verify(transactionListDataManager).getBtcBalance(any())
         verify(transactionListDataManager).getBchBalance(any())
